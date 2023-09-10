ec8a43875ac4f786998c949cff5e882f26a1ba4f
Farruck Ahamed Tusar <frk.tusar@gmail.com>
2023-09-10 23:52:04 +0200

    optimize video player

--- widgets/video_player/__init__.py
+++ widgets/video_player/__init__.py
@@ -1,9 +1,7 @@
 import sys
-
 import ffmpeg
-
-from PySide6.QtCore import Slot
-from PySide6.QtMultimedia import QAudioOutput, QMediaPlayer
+from PySide6.QtCore import Slot, QTimer
+from PySide6.QtMultimedia import QMediaPlayer
 from PySide6.QtMultimediaWidgets import QVideoWidget
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QMessageBox
 from widgets.video_player.ui_video_player import Ui_videoPlayer
@@ -12,12 +10,10 @@ from widgets.video_player.ui_video_player import Ui_videoPlayer
 class VideoPlayer(Ui_videoPlayer, QWidget):
     def __init__(self, main_ui, video_path):
         super().__init__(main_ui)
-
-        self.main_ui = main_ui
         self.ui = Ui_videoPlayer()
         self.ui.setupUi(self)
 
-        self.ui.btn_back.clicked.connect(lambda: self.main_ui.stackedWidget.setCurrentWidget(self.main_ui.page_loadVideos))
+        self.ui.btn_back.clicked.connect(lambda: main_ui.stackedWidget.setCurrentWidget(main_ui.page_loadVideos))
 
         # Use ffmpeg to probe the video file
         probe = ffmpeg.probe(video_path, v="error", select_streams="v:0", show_entries="stream=codec_name")
@@ -29,14 +25,11 @@ class VideoPlayer(Ui_videoPlayer, QWidget):
             dlg = QMessageBox(self)
             dlg.setWindowTitle("Alert")
             dlg.setText(f"Unsupported video codec: {video_codec}")
-            button = dlg.exec_()
-            if button == QMessageBox.Ok:
-                print(f"Unsupported video codec: {video_codec}")
+            dlg.exec_()
             return
 
         self._video_widget = QVideoWidget()
-        frame_player_layout = QVBoxLayout(self.ui.frame_player)
-        frame_player_layout.addWidget(self._video_widget)
+        QVBoxLayout(self.ui.frame_player).addWidget(self._video_widget)
 
         self._player = QMediaPlayer()
         self._player.errorOccurred.connect(self._player_error)
@@ -46,9 +39,6 @@ class VideoPlayer(Ui_videoPlayer, QWidget):
 
         self._player.setSource(video_path)
         self._player.setVideoOutput(self._video_widget)
-
-        self._audio_output = QAudioOutput()
-        self._player.setAudioOutput(self._audio_output)
 
         self._play_action = self.ui.btn_play
         self._play_action.clicked.connect(self._player.play)
@@ -63,6 +53,13 @@ class VideoPlayer(Ui_videoPlayer, QWidget):
         self._play_action.setEnabled(state != QMediaPlayer.PlayingState)
         self._stop_action.setEnabled(state != QMediaPlayer.StoppedState)
 
+        duration = self._player.duration()
+        position = self._player.position()
+        formatted_position = self.format_time(position)
+        formatted_duration = self.format_time(duration)
+        self.ui.label_time.setText(f"{formatted_position}/{formatted_duration}")
+        self.ui.slider_time.setValue(position)
+
     @Slot("QMediaPlayer::Error", str)
     def _player_error(self, error, error_string):
         print(error_string, file=sys.stderr)
@@ -75,3 +72,19 @@ class VideoPlayer(Ui_videoPlayer, QWidget):
     def _ensure_stopped(self):
         if self._player.playbackState() != QMediaPlayer.StoppedState:
             self._player.stop()
+
+    @Slot("qint64")
+    def update_video_position(self, position):
+        duration = self._player.duration()
+        formatted_position = self.format_time(position)
+        formatted_duration = self.format_time(duration)
+        self.ui.label_time.setText(f"{formatted_position}/{formatted_duration}")
+        self.ui.slider_time.setValue(position)
+
+    def format_time(self, milliseconds):
+        total_seconds = milliseconds // 1000
+        milliseconds_remainder = milliseconds % 1000
+        seconds = total_seconds % 60
+        minutes = (total_seconds // 60) % 60
+        hours = total_seconds // 3600
+        return f"{hours:02}:{minutes:02}:{seconds:02}.{milliseconds_remainder:03}"

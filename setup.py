7382f92d5b6458e7a751d0bba7d2919ca8378ee2
Farruck Ahamed Tusar <93766181+farruck-tusar@users.noreply.github.com>
2023-11-22 00:27:01 +0100

    Update setup.py
    - add feature for dist
    - improvement of build packaging

--- setup.py
+++ setup.py
@@ -9,17 +9,50 @@ build_exe_options = {
     "zip_include_packages": [],
 }
 
-target = Executable(
-    script="main.py",
-    base = "Win32GUI" if sys.platform == "win32" else None,
-    icon="resources/icon.ico"
-)
+directory_table = [
+    ("ProgramMenuFolder", "TARGETDIR", "."),
+    ("MyProgramMenu", "ProgramMenuFolder", "MYPROG~1|My Program"),
+]
+
+msi_data = {
+    "Directory": directory_table,
+    "ProgId": [
+        ("Prog.Id", None, None, "This is a description", "IconId", None),
+    ],
+    "Icon": [
+        ("IconId", "resources/icon.ico"),
+    ],
+}
+
+bdist_msi_options = {
+    "add_to_path": True,
+    "data": msi_data
+}
+
+bdist_dmg_options = {
+
+}
+
+executables = [
+    Executable(
+        script="main.py",
+        copyright="Copyright (C) 2023 TUC",
+        base="Win32GUI" if sys.platform == "win32" else None,
+        icon="resources/icon.ico",
+        shortcut_name="IFE",
+        shortcut_dir="MyProgramMenu",
+    )
+]
 
 setup(
     name = "IFE Application",
     version = "1.0.0",
     description = "A Video Processing Software Application",
     author = "Farruck Ahamed Tusar",
-    options={"build_exe": build_exe_options},
-    executables = [target]
+    executables = executables,
+    options={
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options,
+        "bdist_dmg": bdist_dmg_options,
+    }
 )
